ad180eb3e579d765b636966c8396b8f243e67bbe
Oruam <oru_catani@hotmail.com>
2020-07-14 11:58:06 -0300

    Projeto criado

---
+++ src/main/java/com/curso/udemy/nelioalves/cursomc/CursoSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.curso.udemy.nelioalves.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/udemy/nelioalves/cursomc/CursoSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.udemy.nelioalves.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
