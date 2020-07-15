9bec9662e9adba31a528c3e442ed8e8c6d0f814a
Oruam <oru_catani@hotmail.com>
2020-07-15 12:24:12 -0300

    Estado e cidade

---
+++ src/main/java/com/curso/udemy/nelioalves/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.curso.udemy.nelioalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.curso.udemy.nelioalves.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
