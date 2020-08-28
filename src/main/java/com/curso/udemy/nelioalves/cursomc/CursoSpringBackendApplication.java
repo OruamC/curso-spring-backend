bfff8b469c8e06d0df5e27151e1ed1ae7e770faf
OruamC <oru_catani@hotmail.com>
2020-08-27 22:17:51 -0300

    Enviando imagem via endpoint

--- src/main/java/com/curso/udemy/nelioalves/cursomc/CursoSpringBackendApplication.java
+++ src/main/java/com/curso/udemy/nelioalves/cursomc/CursoSpringBackendApplication.java
@@ -1,17 +1,11 @@
 package com.curso.udemy.nelioalves.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.curso.udemy.nelioalves.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursoSpringBackendApplication implements CommandLineRunner{
-	
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringBackendApplication.class, args);
@@ -19,7 +13,6 @@ public class CursoSpringBackendApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Temp\\Imagens\\foto.jpg");
 	}
 
 }
